24df8bbb4a252aabe893320e5b77eae1ad0a821e
JC desarrollo <75315685+JoaquinCaviltelli@users.noreply.github.com>
2021-11-01 17:25:33 -0300

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,22 @@
+<?php
+
+    $destino = "joakindel95@gmail.com";
+        
+        
+        $cod = $_POST["cod"];
+        $nombre = $_POST["nombre"];
+        $peso = $_POST["peso"];
+        $url = $_POST["url"];
+        
+$header = "Solicitud de nuevo articulo";
+$asunto = "Solicitud de nuevo articulo";
+
+$mensaje = $cod . ": { \n nombre: " . $nombre . " , \n peso: " . $peso . ", \n urlImg: " . $url . ", \n },";
+
+mail($destino, $asunto, $mensaje, $header);
+
+echo "<script> alert(`mensaje enviado`)</script>";
+
+
+
+?>
